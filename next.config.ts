7457e1a2d2ded8df21f980a83b8fb4b13757f289
rzydeek <rzydeek@proton.me>
2024-12-16 17:18:30 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
   output: "export",
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-    output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/nextjsmarketplace' : '',
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
+  basePath:  process.env.DEPLOYED_GITHUB_PATH || ''
+    
   images: {
     unoptimized: true,
   },

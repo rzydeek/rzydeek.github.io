c8aebd420eaa99ff9bb11cd384d65bba4881bece
rzydeek <rzydeek@proton.me>
2024-12-16 17:12:15 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,8 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+    output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/nextjsmarketplace' : ''
 
   /**
    * Disable server-based image optimization. Next.js does not support
